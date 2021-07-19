057d620b69e20e0c892b93440014525dc3a1e595
Kamil <kamil.albrycht.33@gmail.com>
2021-07-19 18:10:52 +0200

    formating old code partitions

--- src/EventBus.ts
+++ src/EventBus.ts
@@ -1,26 +1,26 @@
 class EventBus {
-    eventHandlers = {}
-    constructor() {
-        this.initBuiltInEvents()
+  eventHandlers = {};
+  constructor() {
+    this.initBuiltInEvents();
+  }
+  on(eventName, eventHandler) {
+    if (!this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName] = [];
+      this.eventHandlers[eventName].push(eventHandler);
+    } else {
+      this.eventHandlers[eventName].push(eventHandler);
     }
-    on(eventName, eventHandler) {
-        if(!this.eventHandlers[eventName]) {
-            this.eventHandlers[eventName] = []
-            this.eventHandlers[eventName].push(eventHandler)
-        } else {
-            this.eventHandlers[eventName].push(eventHandler)
-        }
+  }
+  emit(eventName, props = {}) {
+    if (this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName].forEach((func) => {
+        func(props);
+      });
     }
-    emit(eventName, props = {}) {
-        if(this.eventHandlers[eventName]) {
-            this.eventHandlers[eventName].forEach(func => {
-                func(props)
-            });
-        }
-    }
-    initBuiltInEvents() {
-        this.eventHandlers['click'] = []
-    }
+  }
+  initBuiltInEvents() {
+    this.eventHandlers["click"] = [];
+  }
 }
-const eventBus = new EventBus()
-export default eventBus
+const eventBus = new EventBus();
+export default eventBus;
